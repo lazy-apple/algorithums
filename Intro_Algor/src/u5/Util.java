18e0e4451e07de93766651f8f54e01a55f85259c
L_aZY <18846171630@163.com>
2018-09-14 16:13:22 +0800

    提交 工具类 代码

--- Intro_Algor/src/u5/Util.java
+++ Intro_Algor/src/u5/Util.java
@@ -49,12 +49,5 @@ public class Util {
         }
     }
 
-    public static void main(String[] args) {
-        int a[] = new int[]{1,2,3,4,5};
-        int ra[] = new int[]{20,19,18,10,11};
-        sort(ra,a);
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
-    }
+
 }
