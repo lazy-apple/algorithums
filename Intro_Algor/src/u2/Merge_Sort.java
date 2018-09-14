7ee701c3f6152cbc9a0ec19686d421f40782bffb
L_aZY <18846171630@163.com>
2018-09-14 14:58:45 +0800

    归并排序

---
+++ Intro_Algor/src/u2/Merge_Sort.java
@@ -0,0 +1,81 @@
+package u2;
+
+/**
+ * 归并排序
+ * @author LaZY（李志一）
+ * @data 2018/9/5 - 16:04
+ */
+public class Merge_Sort {
+    /***
+     *
+     * 归并排序
+     *
+     *  1.将数组分为两部分
+     *  2.将每一部分的数组归并排序
+     *  3.将两部分合并
+     *
+     * @param a 待排序的数组
+     * @param p 数组的起始位置(索引)
+     * @param r 数组的末尾位置(索引)
+     */
+    public static void merge_sort(int a[],int p,int r){
+        if (p<r){
+            int q = (p + r)/2;
+            merge_sort(a,p,q);
+            merge_sort(a,q+1,r);
+            merge(a,p,q,r);
+        }
+    }
+
+    /***
+     * 归并排序的外部方法
+     * @param a
+     */
+    public static void merge_sort(int a[]){
+        int p = 0;
+        int r = a.length-1;
+        merge_sort(a,p,r);
+    }
+
+    /***
+     * 归并排序的合并
+     *
+     *  1.将（两部分排完序的）数组分成两个数组进行排序
+     *  2.在每个数组的最后面加上哨兵（一个很大的数，用来标识数组到了尽头）
+     *  3.对每个数组最上面的数字进行比较，较小的复制到最初的数组中（没分裂的数组）
+     *
+     * @param a 待排序的数组（分界点前后必须已经排好序）
+     * @param p 排序的起始位置
+     * @param q 排序的分界位置
+     * @param r 排序的末尾位置
+     *
+     */
+    public static void merge(int a[], int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+        int L[] = new int[n1 + 1];
+        int R[] = new int[n2 + 1];
+        for (int i = 0; i < n1; i++) {
+            L[i] = a[p + i];
+
+        }
+        for (int i = 0; i < n2; i++) {
+            R[i] = a[q + 1 + i];
+        }
+        L[n1] = 9999999;
+        R[n2] = 9999999;
+        int i = 0;
+        int j = 0;
+        for (int k = p; k <= r; k++) {
+            if (L[i] < R[j]) {
+                a[k] = L[i];
+                i++;
+            } else {
+                a[k] = R[j];
+                j++;
+            }
+        }
+    }
+
+
+}
