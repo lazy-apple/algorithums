a941985f9aa45ea90a6f918917a3d3b14f0a9a5c
L_aZY <18846171630@163.com>
2019-01-04 10:31:57 +0800

    提交：测试代码

---
+++ Intro_Algor/src/u7/Test.java
@@ -0,0 +1,15 @@
+package u7;
+
+import util.Util;
+
+/**
+ * @author LaZY（李志一）
+ * @data 2019/1/4 - 10:20
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a[] = {2,8,7,1,3,5,6,4};
+        Quicksort.QUICKSORT(a);
+        Util.print(a);
+    }
+}
