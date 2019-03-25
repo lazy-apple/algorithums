2afb8a907b56e1f042af43b96f16267d18f1a580
LaZY <18846171630@163.com>
2019-03-25 11:33:03 +0800

    add:对比器--code

---
+++ Alg/src/Comparator/Array_Com.java
@@ -0,0 +1,120 @@
+package Comparator;
+
+import java.util.Arrays;
+
+/**
+ * @author LaZY(李志一)
+ * @create 2019-03-25 11:20
+ */
+public class Array_Com {
+
+    /****
+     * 绝对正确方法
+     * @param arr
+     */
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    /***
+     * 随机样本发生器
+     * @param maxSize
+     * @param maxValue
+     * @return
+     */
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    /***
+     * 拷贝数组
+     * @param arr
+     * @return
+     */
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    /***
+     * 比较两数组
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /***
+     * 打印数组
+     * @param arr
+     */
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        init();
+    }
+
+    private static void init() {
+        Methord m = null;
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            m.insertionSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        m.insertionSort(arr);
+        printArray(arr);
+    }
+}
---
+++ Alg/src/Comparator/Methord.java
@@ -0,0 +1,9 @@
+package Comparator;
+
+/**
+ * @author LaZY(李志一)
+ * @create 2019-03-25 11:24
+ */
+public interface Methord {
+    public int[] insertionSort(int arr[]);
+}
